b32bbe9ca32846403d00a686317196a2dd9a62c0
wangadd <1276321779@qq.com>
2019-02-16 15:24:47 +0800

    weixin

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -7,8 +7,23 @@ use App\Http\Controllers\Controller;
 
 class WxController extends Controller
 {
-    public function valid(){
+    protected $redis_weixin_access_token = 'str:weixin_access_token';
+    /**
+     * 首次接入
+     */
+    public function valid()
+    {
         echo $_GET['echostr'];
+    }
+
+    /**
+     * 接收微信服务器事件推送
+     */
+    public function wxEvent()
+    {
+        $data = file_get_contents("php://input");
+        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
 }
 
