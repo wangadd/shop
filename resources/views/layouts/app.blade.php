58e798e0555d3aea8b167a340bc246822b685044
wangadd <1276321779@qq.com>
2019-02-28 05:47:34 +0000

    debug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,7 +72,7 @@
                                 </ul>
                             </li>
                             <li class="dropdown">
-                                <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dhttp%3a%2f%2fshop.comcto.com%2fweixin%2fsns&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">Login</a>
+                                <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dking.tactshan.com%2fweixin%2fgetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">微信登录</a>
                             </li>
                         @endguest
                     </ul>
