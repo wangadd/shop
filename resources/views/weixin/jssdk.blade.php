65fb539ef21905e8cdf3487730a05ce907e0a9e2
wangadd <1276321779@qq.com>
2019-03-01 03:52:12 +0000

    debug

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -5,7 +5,6 @@
 @endsection
 @section('footer')
     @parent
-    <script src="{{URL::asset('/js/weixin/chat.js')}}"></script>
     <script src="https://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
     <script>
         wx.config({
