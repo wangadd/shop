a9fc31830be400998fdf60f20ab7494b17183192
wangadd <1276321779@qq.com>
2019-03-01 06:11:33 +0000

    debug

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>jssdk</h1>
     <button id="btn1">选择照片</button>
+    <button id="btn2">点击扫一扫</button>
 @endsection
 @section('footer')
     @parent
@@ -14,9 +15,30 @@
             timestamp: "{{$data['timestamp']}}", // 必填，生成签名的时间戳
             nonceStr: "{{$data['nonceStr']}}", // 必填，生成签名的随机串
             signature:"{{$data['sign']}}",// 必填，签名
-            jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
+            jsApiList: ['chooseImage','scanQRCode','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
         });
+        wx.ready(function(){
+            $("#btn1").click(function () {
+                wx.chooseImage({
+                    count: 9, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                    success: function (res) {
+                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    }
+                });
+                $('#btn2').click(function () {
+                    wx.scanQRCode({
+                        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                        success: function (res) {
+                            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                        }
+                    });
+                })
+            });
 
+        });
     </script>
 
 @endsection
