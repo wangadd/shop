51f461b26cab1752482f7dd5117bd4db7ec0dfd7
wangadd <1276321779@qq.com>
2019-03-01 03:45:27 +0000

    jssdk

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -9,7 +9,6 @@
 
 @endsection
 <script>
-    $(function () {
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: "{{$data['appid']}}", // 必填，公众号的唯一标识
@@ -18,5 +17,5 @@
             signature:"{{$data['signature']}}",// 必填，签名
             jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
         });
-    })
+    
 </script>
